8e6799da064d400b8bbde034d83c251bb9a7d20d
aprutledge <39936030+aprutledge@users.noreply.github.com>
2020-09-29 21:01:37 -0500

    Added some auth middleware and controllers
    Added some auth middleware and controllers

---
+++ backend/middleware/checkUsername.js
@@ -0,0 +1,20 @@
+const db = require("../models");
+const User = db.user;
+
+checkUsername = (req, res, next) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((user) => {
+    if (user) {
+      res.status(400).send({
+        message: "Failed! Username is already in use!",
+      });
+      return;
+    }
+    next();
+  });
+};
+
+module.exports = checkUsername;
---
+++ backend/middleware/index.js
@@ -0,0 +1,9 @@
+const authJwt = require("./authJwt");
+const checkUsername = require("./checkUsername");
+const checkEmail = require("./checkEmail");
+
+module.exports = {
+  authJwt,
+  checkUsername,
+  checkEmail,
+};
